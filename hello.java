4424d2c65971133a0c5608c02065846856ee2877
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2023-12-02 23:49:48 +0530

    added an feature to ask the user his name

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class hello
 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");    
         String name = sc.next();
         System.out.print("hello ");
         System.out.println(name);
